9b86f3ebd1e0ca47ef8fe65cbd98b6cbb1565aa8
Tony Won <tonywon89@gmail.com>
2017-10-10 15:56:12 -0700

    Add basic animation for navbar menu item

--- frontend/Components/authentication/AuthForms.jsx
+++ frontend/Components/authentication/AuthForms.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoginForm from './LoginForm.jsx'
 import RegisterForm from './RegisterForm.jsx'
-
+import { CSSTransitionGroup } from 'react-transition-group'
 
 class AuthForms extends React.Component {
   constructor(props) {
@@ -10,11 +10,13 @@ class AuthForms extends React.Component {
       currentUser: props.authentication.currentUser,
       loginForm: false,
       registerForm: false,
+      dropdownVisible: false
     }
 
     this.toggleRegisterForm = this.toggleRegisterForm.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
     this.toggleLoginForm = this.toggleLoginForm.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -36,13 +38,49 @@ class AuthForms extends React.Component {
     this.props.logout();
   }
 
+  toggleDropdown() {
+    if (this.state.dropdownVisible === false) {
+      this.setState({ dropdownVisible: true });
+    } else {
+      this.setState({ dropdownVisible: false });
+    }
+  }
+
   render() {
     const loginLink = <a href="#" onClick={this.toggleLoginForm}>Login</a>;
     const registerLink = <a href="#" onClick={this.toggleRegisterForm}>Register</a>;
+    let menuItems = this.state.dropdownVisible ? (
+      <div className="dropdown-list">
+        <div key={1}>
+          <span>Profile</span>
+        </div>
+        <div key={2}>
+          <span>Settings</span>
+        </div>
+        <div key={3}>
+          <span onClick={this.handleLogout}>Logout</span>
+        </div>
+      </div>
+    ) : "";
     if (this.state.currentUser) {
       return (
-        <div>
-          {this.state.currentUser.username}  <i className="fa fa-caret-down"></i>    <button onClick={this.handleLogout}>Logout</button>
+        <div className="dropdown-container" onClick={this.toggleDropdown}>
+          <div>
+            {this.state.currentUser.username} <i className="fa fa-caret-down"></i>
+          </div>
+
+            <CSSTransitionGroup
+              transitionName="example"
+              // transitionAppear={true}
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}
+              // transitionEnter={false}
+              // transitionLeave={false}
+              >
+              {menuItems}
+
+            </CSSTransitionGroup>
+
         </div>
       );
     } else {
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
           presets: ['es2015', 'react'] // Tells Babel what syntaxes to translate
         },
 
-      }
+      },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ],
