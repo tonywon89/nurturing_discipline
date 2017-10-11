8af9e071ec781a43fd6a30e361db20416f23cbf3
Tony Won <tonywon89@gmail.com>
2017-10-11 14:22:07 -0700

    Add css reset and custom button style

--- frontend/Components/authentication/AuthForms.jsx
+++ frontend/Components/authentication/AuthForms.jsx
@@ -6,13 +6,21 @@ import Modal from 'react-modal';
 
 // @TODO: change this to make it suit my needs
 const customStyles = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(0, 0, 0, 0.75)'
+  },
   content : {
     top                   : '50%',
     left                  : '50%',
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    transform             : 'translate(-50%, -50%)',
   }
 };
 
