7888da5b0a76f1baf37e72dc1518915e6f24ef51
Tony Won <tonywon89@gmail.com>
2017-10-12 18:44:41 -0700

    Remove first and last name requirements, update configuration to use SASS

--- frontend/Components/authentication/RegisterForm.jsx
+++ frontend/Components/authentication/RegisterForm.jsx
@@ -8,31 +8,17 @@ class RegisterForm extends React.Component {
       username: "",
       password: "",
       passwordConfirmation: "",
-      firstName: "",
-      lastName: ""
     }
 
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handlePassConfirmChange = this.handlePassConfirmChange.bind(this);
-    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
-    this.handleLastNameChange = this.handleLastNameChange.bind(this);
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
   }
 
   handleRegisterSubmit(event) {
     event.preventDefault();
-
-    if (this.state.firstName.trim() === "") {
-      alert("A first name must be entered for registration");
-      return;
-    }
-
-    if (this.state.lastName.trim() === "") {
-      alert("A last name must be entered for registration");
-      return;
-    }
 
     if (this.state.email.trim() === "") {
       alert("An email must be entered for registration");
@@ -52,6 +38,8 @@ class RegisterForm extends React.Component {
     if (this.state.password.trim() !== this.state.passwordConfirmation) {
       alert("The passwords don't match.");
     }
+
+    this.props.closeModal();
 
     this.props.register({
       firstName: this.state.firstName,
@@ -78,21 +66,9 @@ class RegisterForm extends React.Component {
     this.setState({ passwordConfirmation: event.target.value });
   }
 
-  handleFirstNameChange(event) {
-    this.setState({ firstName: event.target.value });
-  }
-
-  handleLastNameChange(event) {
-    this.setState({ lastName: event.target.value });
-  }
-
   render() {
     return (
       <form onSubmit={this.handleRegisterSubmit}>
-        <label>First Name</label>
-        <input type="text" name="first_name" placeholder="First Name" value={this.state.firstName} onChange={this.handleFirstNameChange} /><br/>
-        <label>Last Name</label>
-        <input type="text" name="last_name" placeholder="Last Name" value={this.state.lastName} onChange={this.handleLastNameChange} /><br/>
         <label>Email</label>
         <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange} /><br/>
         <label>Username</label>
