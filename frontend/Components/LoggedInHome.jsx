47caebd627724c172642e27cde1290258f51f655
Tony Won <tonywon89@gmail.com>
2017-12-15 17:52:02 -0800

    Show default page for home route

---
+++ frontend/Components/LoggedInHome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LoggedInHome extends React.Component {
+  render() {
+    return (
+      <div className="home-page">
+        <h3>Welcome to Nurturing Discpline</h3>
+        <div>
+          Navigate through the sidebar on the left!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoggedInHome;
